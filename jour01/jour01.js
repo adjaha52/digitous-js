681327a343e70932409cd84887d61a65d6477045
Djahan <jessicange74@yaghoo.fr>
2021-06-14 16:39:33 +0200

    jour1-debut

---
+++ jour01/jour01.js
@@ -0,0 +1,23 @@
+// Hello world
+console.log(" hello world")
+
+//string
+var test = "My name is Ange";
+console.log(test);
+
+// concatenation
+var name= "Ange";
+console.log("nice to meet you + name");
+
+// string length
+var testLength= "I m very long"
+console.log(testLenght.length);
+
+// replace
+var food= "croissant is meh";
+food = food.replacve("meh", "so good");
+console.log(food);
+
+// UP et Down
+var basic= "this is cool";
+var basicUp= 
