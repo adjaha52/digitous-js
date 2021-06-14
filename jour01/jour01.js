bf99562e684b8c7b51842d34c274602e99916999
Djahan <jessicange74@yaghoo.fr>
2021-06-14 17:32:27 +0200

    fin jour1

--- jour01/jour01.js
+++ jour01/jour01.js
@@ -1,5 +1,5 @@
 // Hello world
-console.log(" hello world")
+console.log("hello world");
 
 //string
 var test = "My name is Ange";
@@ -7,22 +7,22 @@ console.log(test);
 
 // concatenation
 var name= "Ange";
-console.log("nice to meet you + name");
+console.log("nice to meet you" + " name ");
 
 // string length
-var testLength= "I m very long"
+var testLength= "I m very long";
 console.log(testLength.length);
 
 // replace
 var food= "croissant is meh";
-food = food.replacve("meh", "so good");
+food = food.replace("meh", "so good");
 console.log(food);
 
 // UP et Down
 var basic= "this is cool";
 var basicUp= basic.toUpperCase;
 var basicDown= basicUp.toLowercase;
-console.log( basic, basicUp?basicDown);
+console.log( basic, basicUp , basicDown);
 
 // slip
 var world= "banana";
@@ -30,6 +30,6 @@ var letters= world.split("");
 console.log(letters);
 
 // template 
-var age= "21";
-var template ="i am + age + years old";
+var age= 21;
+var template ="i am" + ""+age+ ""+ "years old";
 console.log( template );
