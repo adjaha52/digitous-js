d7aedae0addcc37d6ee765a85f9b8635c06ded51
Djahan <jessicange74@yaghoo.fr>
2021-06-23 13:58:05 +0200

    jour5

--- jour5.js
+++ jour5.js
@@ -1,22 +1,23 @@
 // exo 1
 function calculate (entier1, entier2, operateur){
     if ( operateur === "+") {
-        console.log("a operateur b");
+        console.log("entier1 + entier2");
     }
-        if( operatuer==="-"){
-            console.log("a operateur b");
+        if( operateur==="-"){
+            console.log("entier1 - entier2");
         }
         if(operateur ==="/"){
-            console.log("a operateur b");
+            console.log("entier1 / entier2");
         }    
         if (operateur ==="%"){
-            console.log("a operateur b");
+            console.log("entier1 % entier2");
         } 
     else {
         console.log("rien à signaler");
     }        
 }
-var entier1=4;
-var entier2=3;
+var entier1 = 4;
+var entier2 = 3;
 var operateur= "+"; 
-calculate (entier1, entier2, operateur);
+calculate (4, 3,"+");
+
